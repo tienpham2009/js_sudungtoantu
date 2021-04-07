e34f4470f5e924ddc3eeef5c4d53e51ce43fc0bc
tienpham2009 <tiendx2009@gmail.com>
2021-04-07 10:50:13 +0700

    su dung toan tu

---
+++ sudungtoantu.js
@@ -0,0 +1,10 @@
+let inputWidth = prompt("Enter the width");
+document.write("The width:" +inputWidth);
+document.write("<br>");
+let inputHeigth= prompt("Enter the Heigth");
+document.write("The heigth:" +inputHeigth);
+document.write("<br>")
+let width = parseInt(inputWidth);
+let heigth = parseInt(inputHeigth);
+let area = width*heigth;
+document.write("Area is : =" +area);
